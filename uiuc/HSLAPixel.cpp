385061d4bcb0259e629d5586bd296ed8e603aee4
arsm10 <71931953+arsm10@users.noreply.github.com>
2020-09-27 19:14:03 +0200

    HSLA .cpp and .h

---
+++ uiuc/HSLAPixel.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file HSLAPixel.cpp
+ * Implementation of the HSLAPixel class for use in with the PNG library.
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#include <cmath>
+#include <iostream>
+#include "HSLAPixel.h"
+using namespace std;
+
+namespace uiuc {
+    HSLAPixel::HSLAPixel() {
+        h = 0;
+        s = 0;
+        l = 1.0;
+        a = 1.0;
+    }
+
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = 1.0;
+    }
+
+    HSLAPixel::HSLAPixel(double hue, double saturation, double luminance, double alpha) {
+        h = hue;
+        s = saturation;
+        l = luminance;
+        a = alpha;
+    }
+
+    HSLAPixel& HSLAPixel::operator=(HSLAPixel const& other) {
+        this->h = other.h;
+        this->s = other.s;
+        this->l = other.l;
+        this->a = other.a;
+        return *this;
+    }
+
+    bool HSLAPixel::operator== (HSLAPixel const& other) const {
+        // thank/blame Wade for the following function
+        if (fabs(a - other.a) > 0.01) { return false; }
+        if (a == 0) { return true; }
+
+        if (fabs(l - other.l) > 0.01) { return false; }
+        if (l == 0 || l == 1) { return true; }
+
+        if (fabs(s - other.s) > 0.01) { return false; }
+        if (s == 0) { return true; }
+
+        if (fabs(h - other.h) < 0.5 || fabs(h - other.h) > 359.5) { return true; }
+        else { return false; }
+    }
+
+    bool HSLAPixel::operator!= (HSLAPixel const& other) const {
+        return !(*this == other);
+    }
+
+    std::ostream& operator<<(std::ostream& out, HSLAPixel const& pixel) {
+        out << "(" << pixel.h << ", " << pixel.s << ", " << pixel.l << (pixel.a != 1 ? ", " + std::to_string(pixel.a) : "") << ")";
+
+        return out;
+    }
+
+
+}
---
+++ uiuc/HSLAPixel.h
@@ -0,0 +1,30 @@
+/**
+ * @file HSLAPixel.h
+ *
+ * @author University of Illinois CS 225 Course Staff
+ * @version 2018r1-lab1 - Updated for CS 400
+ */
+
+#pragma once
+
+#include <iostream>
+#include <sstream>
+
+namespace uiuc {
+    class HSLAPixel {
+    public:
+        double h;
+        double s;
+        double l;
+        double a;
+        HSLAPixel();
+        HSLAPixel(double hue, double saturation, double luminance);
+        HSLAPixel(double hue, double saturation, double luminance, double alpha);
+
+        HSLAPixel& operator= (HSLAPixel const& other);
+        bool operator== (HSLAPixel const& other) const;
+        bool operator!= (HSLAPixel const& other) const;
+    };
+    std::ostream& operator<<(std::ostream& out, HSLAPixel const& pixel);
+    std::stringstream& operator<<(std::stringstream& out, HSLAPixel const& pixel);
+}
